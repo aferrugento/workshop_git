d6315c2ae4508eb31b8c806afdb4a52a29456782
Adriana Ferrugento <adrianaferrugento@gmail.com>
2014-03-12 16:48:27 +0000

    Added main function

--- teste.py
+++ teste.py
@@ -1,2 +1,6 @@
-print "O Tiago é um deficiente mental"
-print "És tão fofinha *.*"
+def hello():
+	print "O Tiago é um deficiente mental"
+	print "És tão fofinha *.*"
+
+if __name__ == "__main__":
+	hello()
